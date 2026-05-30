d4d614e0b93b3e1f234baf0f6d79360d1450aefc
Alpyg <alpyg@pm.me>
2026-05-29 23:02:47 -0400

    add client

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
+#[cfg(feature = "client")]
+pub mod client;
 pub mod modules;
 
 pub mod prelude {
+    #[cfg(feature = "client")]
+    pub use crate::client::*;
+
     pub use crate::modules::character::*;
     pub use crate::modules::player::*;
     pub use crate::modules::restriction::*;
